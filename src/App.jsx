d1f1b9be27c5337d815d25d7c20ebbc5812eff3a
Ankit Sharma <Ankitsharma7805@gmail.com>
2025-01-12 17:48:32 +0530

    atleast

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,10 @@
-import { useEffect, useState } from "react";
-import { getAuth,  onAuthStateChanged, signOut } from "firebase/auth";
-import { app } from './firebase';
-import Signup from "./pages/SignUp";
-import Signin from "./pages/Signin";
+import React from 'react'
 import './App.css'
 
-
-const auth = getAuth(app);
-
-function App() { 
-  const [user, setUser] = useState(null);
-  useEffect(()=> {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        //Yes, You are logged in
-        setUser(user);
-        // console.log("Hello",user);
-      }else {
-        // User is logged out
-        console.log("You are logout");
-        setUser(null);
-      }
-  });
-  },[]);
-  if(user === null){
-    return(
-      <>
-        <div className="App">
-          <Signup/>
-          <Signin/>
-        </div>
-      </>
-    )
-  }
-
-    return (
-      <>
-        <div className="App">
-          <h1>Hello{user.email}</h1>
-          <button onClick={()=>signOut(auth)}>LogOut</button>
-        </div>
-      </>
-    );
-  }
+const App = () => {
+  return (
+    <div>FireBase</div>
+  )
+}
 
 export default App
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,3 +1,17 @@
+//--This FireBase for App_1.jsx and App_3.jsx Only
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCJs-pQSpI94F63n4hEb_1B1b8ddXesH2Y",
+//   authDomain: "app-f60d0.firebaseapp.com",
+//   projectId: "app-f60d0",
+//   storageBucket: "app-f60d0.firebasestorage.app",
+//   messagingSenderId: "942355380101",
+//   appId: "1:942355380101:web:9bb8c00bdb20a2b9dc36ec",
+//   databaseURL:"https://app-f60d0-default-rtdb.firebaseio.com",
+// };
+
+// export const app = initializeApp(firebaseConfig);
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
