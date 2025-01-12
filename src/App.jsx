971bc11251f377bf7ff39c66057980fd6bceb6f9
Ankit Sharma <Ankitsharma7805@gmail.com>
2025-01-12 10:48:11 +0530

    app.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
-import { getAuth,  onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { getAuth,  onAuthStateChanged, signOut } from "firebase/auth";
 import { app } from './firebase';
 import Signup from "./pages/SignUp";
 import Signin from "./pages/Signin";
 import './App.css'
-import { useEffect } from "react";
+
 
 const auth = getAuth(app);
 
 function App() { 
+  const [user, setUser] = useState(null);
   useEffect(()=> {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         //Yes, You are logged in
-        console.log("Hello",user);
+        setUser(user);
+        // console.log("Hello",user);
       }else {
         // User is logged out
-        console.log("You are logout")
+        console.log("You are logout");
+        setUser(null);
       }
   });
   },[]);
-
-    return (
+  if(user === null){
+    return(
       <>
         <div className="App">
           <Signup/>
           <Signin/>
+        </div>
+      </>
+    )
+  }
+
+    return (
+      <>
+        <div className="App">
+          <h1>Hello{user.email}</h1>
+          <button onClick={()=>signOut(auth)}>LogOut</button>
         </div>
       </>
     );
